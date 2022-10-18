6b10e93a644c75a84318d976b0bb03ba5288bc61
keson.an <keson.an@yijinin.com>
2022-10-18 11:32:43 +0800

    Remove unused code

--- proxy/const.go
+++ proxy/const.go
@@ -2,12 +2,3 @@ package proxy
 
 const cpListen = "127.0.0.1:9999"
 const socketAddress = "/tmp/sidecar.sock"
-
-const grpcAuthorizationKey = "grpc-authorization"
-const grpcAuthorizationMethodKey = "grpc-signing-method"
-
-const signingMethodECDSA = "SigningMethodECDSA"
-const signingMethodEd25519 = "SigningMethodEd25519"
-const signingMethodHMAC = "SigningMethodHMAC"
-const signingMethodRSA = "SigningMethodRSA"
-const signingMethodRSAPSS = "SigningMethodRSAPSS"
