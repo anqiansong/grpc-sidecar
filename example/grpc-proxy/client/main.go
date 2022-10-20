822a0ea822188677bd8a38d35cda60db4aab0dd8
keson.an <keson.an@yijinin.com>
2022-10-20 11:17:07 +0800

    Add insecure

--- example/grpc-proxy/client/main.go
+++ example/grpc-proxy/client/main.go
@@ -7,21 +7,24 @@ import (
 
 	"github.com/anqiansong/grpc-sidecar/example/grpc-proxy/pb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/metadata"
 )
 
 func main() {
-	conn, err := grpc.Dial("127.0.0.1:9001", grpc.WithChainUnaryInterceptor(func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
-		md, _ := metadata.FromOutgoingContext(ctx)
-		if len(md) != 0 {
-			values := md.Get("grpc-proxy-date")
-			if len(values) > 0 {
-				fmt.Println("grpc-proxy-date:", values[0])
+	conn, err := grpc.Dial("127.0.0.1:9001",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithChainUnaryInterceptor(func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+			md, _ := metadata.FromOutgoingContext(ctx)
+			if len(md) != 0 {
+				values := md.Get("grpc-proxy-date")
+				if len(values) > 0 {
+					fmt.Println("grpc-proxy-date:", values[0])
+				}
 			}
-		}
 
-		return invoker(ctx, method, req, reply, cc, opts...)
-	}))
+			return invoker(ctx, method, req, reply, cc, opts...)
+		}))
 	if err != nil {
 		log.Fatalln(err)
 	}
