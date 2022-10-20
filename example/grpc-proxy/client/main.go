b2fe6e408841e25f7c07da0dce7ec34af6a3bea4
keson.an <keson.an@yijinin.com>
2022-10-20 10:10:56 +0800

    Add grpc-proxy example

---
+++ example/grpc-proxy/client/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/anqiansong/grpc-sidecar/example/grpc-proxy/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+func main() {
+	conn, err := grpc.Dial("127.0.0.1:9001", grpc.WithChainUnaryInterceptor(func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+		md, _ := metadata.FromOutgoingContext(ctx)
+		if len(md) != 0 {
+			values := md.Get("grpc-proxy-date")
+			if len(values) > 0 {
+				fmt.Println("grpc-proxy-date:", values[0])
+			}
+		}
+
+		return invoker(ctx, method, req, reply, cc, opts...)
+	}))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client := pb.NewGreetServiceClient(conn)
+	ctx := context.Background()
+	resp, err := client.Echo(ctx, &pb.Req{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(resp.Out)
+}
---
+++ example/grpc-proxy/server/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"time"
+
+	"github.com/anqiansong/grpc-sidecar/example/grpc-proxy/pb"
+	"github.com/mwitkow/grpc-proxy/proxy"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+func main() {
+	go listenProxy()
+	listenServer()
+}
+
+func listenProxy() {
+	th := proxy.TransparentHandler(func(ctx context.Context, fullMethodName string) (context.Context, *grpc.ClientConn, error) {
+		conn, err := grpc.Dial("127.0.0.1:9001")
+		if err != nil {
+			return nil, nil, err
+		}
+
+		newCtx := metadata.NewOutgoingContext(ctx, metadata.New(map[string]string{
+			"grpc-proxy-date": time.Now().Format("2006-01-02 15:04:05"),
+		}))
+		return newCtx, conn, nil
+	})
+
+	serviceHandlerOption := grpc.UnknownServiceHandler(th)
+	l, err := net.Listen("tcp", ":9002")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	server := grpc.NewServer(serviceHandlerOption)
+	defer server.GracefulStop()
+	server.Serve(l)
+}
+
+func listenServer() {
+	l, err := net.Listen("tcp", ":9001")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	server := grpc.NewServer()
+	pb.RegisterGreetServiceServer(server, &Greet{})
+	defer server.GracefulStop()
+	server.Serve(l)
+}
+
+type Greet struct {
+	pb.UnimplementedGreetServiceServer
+}
+
+func (g *Greet) Echo(ctx context.Context, req *pb.Req) (*pb.Resp, error) {
+	return &pb.Resp{Out: req.In}, nil
+}
